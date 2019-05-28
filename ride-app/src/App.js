59c83acb0c0becc45930e04ccb3abd1908fca288
Ash <ashley.cruz.crespo@gmail.com>
2019-05-28 17:53:11 -0400

    working on the login page

--- ride-app/src/App.js
+++ ride-app/src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link, NavLink, withRouter } from 'react-router-dom';
+import {connect} from 'react-redux';
+import Login from './Components/Login';
+import Home from './Components/Home';
+import './styles/App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+  render(){
+    return (
+      <Router>
+      <div className="App">
+        <nav className="navigation-bar">
+          <div className="nav-links"> 
+          <NavLink exact to="/">Home</NavLink>
+    
+          </div>
+          <div>
+            {!this.props.isLoggedIn ? (
+               <Link to="/LoginPage">Log In</Link>
+            ) : <Link to="/" onClick={logout}>Log Out</Link> 
+            }
+          </div>
+        </nav>
+
+        <Route exact path='/' component={Home} />
+        <Route path="/login" component={Login} />
+      </div>
+      </Router>
+    );
+  }
 }
 
-export default App;
+function logout() {
+  localStorage.removeItem('token')
+    window.location.reload(true);
+}
+
+const mapStateToProps = state => ({
+    isLoggedIn: state.isLoggedIn
+})
+
+export default connect(mapStateToProps, {} )(App);
+
+
+
+
